720291dc8e0e8f12d105dc215cdd165bb13b2354
OleKjaerulff <okjaerulff@sund.ku.dk>
2017-10-20 22:10:03 +0200

    Added SharedPreference test (worked) But hvae used TextView fields for user entry by mistake (do not accept input) so aborting version. But can be used if medicindata are read in from file.

--- PillPilot-v2/MainActivity.cs
+++ PillPilot-v2/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
+using Android.Preferences;
 
 namespace PillPilot_v2
 {
@@ -13,6 +15,22 @@ namespace PillPilot_v2
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            TextView morgenNavn1 = FindViewById<TextView>(Resource.Id.morgenNavn1);
+
+
+            string morgenNavn1val = "Ole";
+            ISharedPreferences prefs = PreferenceManager.GetDefaultSharedPreferences(this);
+            ISharedPreferencesEditor editor = prefs.Edit();
+            editor.PutString("morgenNavn1", morgenNavn1val);
+            // editor.Commit();    // applies changes synchronously on older APIs
+            editor.Apply();        // applies changes asynchronously on newer APIs
+
+            
+
+            // mInt = prefs.GetInt("key_for_my_int_value", < default value >);
+            morgenNavn1val = prefs.GetString("morgenNavn1", morgenNavn1val);
+            morgenNavn1.Text = morgenNavn1val;
 
         }
     }
