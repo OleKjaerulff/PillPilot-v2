feade9a05ae841b8b67c0eb512ad58e47539406f
OleKjaerulff <okjaerulff@sund.ku.dk>
2017-10-20 15:05:58 +0200

    All fields entered in grid and named

--- PillPilot-v2/Resources/Resource.Designer.cs
+++ PillPilot-v2/Resources/Resource.Designer.cs
@@ -44,17 +44,221 @@ namespace PillPilot_v2
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f040040
+			public const int aftenAlarm1 = 2130968640;
+			
+			// aapt resource value: 0x7f040046
+			public const int aftenAlarm2 = 2130968646;
+			
+			// aapt resource value: 0x7f04003f
+			public const int aftenAntal1 = 2130968639;
+			
+			// aapt resource value: 0x7f040045
+			public const int aftenAntal2 = 2130968645;
+			
+			// aapt resource value: 0x7f04003e
+			public const int aftenDosis1 = 2130968638;
+			
+			// aapt resource value: 0x7f040044
+			public const int aftenDosis2 = 2130968644;
+			
+			// aapt resource value: 0x7f040042
+			public const int aftenHvornår1 = 2130968642;
+			
+			// aapt resource value: 0x7f040047
+			public const int aftenHvornår2 = 2130968647;
+			
+			// aapt resource value: 0x7f04003d
+			public const int aftenNavn1 = 2130968637;
+			
+			// aapt resource value: 0x7f040043
+			public const int aftenNavn2 = 2130968643;
+			
+			// aapt resource value: 0x7f040041
+			public const int aftenTaget1 = 2130968641;
+			
+			// aapt resource value: 0x7f040032
+			public const int blankAften2 = 2130968626;
+			
+			// aapt resource value: 0x7f040033
+			public const int blankAften3 = 2130968627;
+			
+			// aapt resource value: 0x7f040034
+			public const int blankAften4 = 2130968628;
+			
+			// aapt resource value: 0x7f040035
+			public const int blankAften5 = 2130968629;
+			
+			// aapt resource value: 0x7f040036
+			public const int blankAften6 = 2130968630;
+			
+			// aapt resource value: 0x7f04001a
+			public const int blankMiddag2 = 2130968602;
+			
+			// aapt resource value: 0x7f04001b
+			public const int blankMiddag3 = 2130968603;
+			
+			// aapt resource value: 0x7f04001c
+			public const int blankMiddag4 = 2130968604;
+			
+			// aapt resource value: 0x7f04001d
+			public const int blankMiddag5 = 2130968605;
+			
+			// aapt resource value: 0x7f04001e
+			public const int blankMiddag6 = 2130968606;
+			
+			// aapt resource value: 0x7f040002
+			public const int blankMorgen2 = 2130968578;
+			
 			// aapt resource value: 0x7f040003
-			public const int checkBox1 = 2130968579;
+			public const int blankMorgen3 = 2130968579;
+			
+			// aapt resource value: 0x7f040004
+			public const int blankMorgen4 = 2130968580;
+			
+			// aapt resource value: 0x7f040005
+			public const int blankMorgen5 = 2130968581;
+			
+			// aapt resource value: 0x7f040006
+			public const int blankMorgen6 = 2130968582;
 			
 			// aapt resource value: 0x7f040000
 			public const int gridLayout1 = 2130968576;
 			
-			// aapt resource value: 0x7f040001
-			public const int textView1 = 2130968577;
+			// aapt resource value: 0x7f040028
+			public const int middagAlarm1 = 2130968616;
 			
-			// aapt resource value: 0x7f040002
-			public const int textView2 = 2130968578;
+			// aapt resource value: 0x7f04002e
+			public const int middagAlarm2 = 2130968622;
+			
+			// aapt resource value: 0x7f040027
+			public const int middagAntal1 = 2130968615;
+			
+			// aapt resource value: 0x7f04002d
+			public const int middagAntal2 = 2130968621;
+			
+			// aapt resource value: 0x7f040026
+			public const int middagDosis1 = 2130968614;
+			
+			// aapt resource value: 0x7f04002c
+			public const int middagDosis2 = 2130968620;
+			
+			// aapt resource value: 0x7f04002a
+			public const int middagHvornår1 = 2130968618;
+			
+			// aapt resource value: 0x7f040030
+			public const int middagHvornår2 = 2130968624;
+			
+			// aapt resource value: 0x7f040025
+			public const int middagNavn1 = 2130968613;
+			
+			// aapt resource value: 0x7f04002b
+			public const int middagNavn2 = 2130968619;
+			
+			// aapt resource value: 0x7f040029
+			public const int middagTaget1 = 2130968617;
+			
+			// aapt resource value: 0x7f04002f
+			public const int middagTaget2 = 2130968623;
+			
+			// aapt resource value: 0x7f040010
+			public const int morgenAlarm1 = 2130968592;
+			
+			// aapt resource value: 0x7f040016
+			public const int morgenAlarm2 = 2130968598;
+			
+			// aapt resource value: 0x7f04000f
+			public const int morgenAntal1 = 2130968591;
+			
+			// aapt resource value: 0x7f040015
+			public const int morgenAntal2 = 2130968597;
+			
+			// aapt resource value: 0x7f04000e
+			public const int morgenDosis1 = 2130968590;
+			
+			// aapt resource value: 0x7f040014
+			public const int morgenDosis2 = 2130968596;
+			
+			// aapt resource value: 0x7f040012
+			public const int morgenHvornår1 = 2130968594;
+			
+			// aapt resource value: 0x7f040018
+			public const int morgenHvornår2 = 2130968600;
+			
+			// aapt resource value: 0x7f04000d
+			public const int morgenNavn1 = 2130968589;
+			
+			// aapt resource value: 0x7f040013
+			public const int morgenNavn2 = 2130968595;
+			
+			// aapt resource value: 0x7f040011
+			public const int morgenTaget1 = 2130968593;
+			
+			// aapt resource value: 0x7f040017
+			public const int morgenTaget2 = 2130968599;
+			
+			// aapt resource value: 0x7f040031
+			public const int titleAFTEN = 2130968625;
+			
+			// aapt resource value: 0x7f04003a
+			public const int titleALARMaften = 2130968634;
+			
+			// aapt resource value: 0x7f040022
+			public const int titleALARMmiddag = 2130968610;
+			
+			// aapt resource value: 0x7f04000a
+			public const int titleALARMmorgen = 2130968586;
+			
+			// aapt resource value: 0x7f040021
+			public const int titleANATLmiddag = 2130968609;
+			
+			// aapt resource value: 0x7f040039
+			public const int titleANTALaften = 2130968633;
+			
+			// aapt resource value: 0x7f040009
+			public const int titleANTALmorgen = 2130968585;
+			
+			// aapt resource value: 0x7f040038
+			public const int titleDOSISaften = 2130968632;
+			
+			// aapt resource value: 0x7f040020
+			public const int titleDOSISmiddag = 2130968608;
+			
+			// aapt resource value: 0x7f040008
+			public const int titleDOSISmorgen = 2130968584;
+			
+			// aapt resource value: 0x7f04003c
+			public const int titleHVORNÅRaften = 2130968636;
+			
+			// aapt resource value: 0x7f040024
+			public const int titleHVORNÅRmiddag = 2130968612;
+			
+			// aapt resource value: 0x7f04000c
+			public const int titleHVORNÅRmorgen = 2130968588;
+			
+			// aapt resource value: 0x7f040019
+			public const int titleMIDDAG = 2130968601;
+			
+			// aapt resource value: 0x7f040001
+			public const int titleMORGEN = 2130968577;
+			
+			// aapt resource value: 0x7f040037
+			public const int titleNAVNaften = 2130968631;
+			
+			// aapt resource value: 0x7f04001f
+			public const int titleNAVNmiddag = 2130968607;
+			
+			// aapt resource value: 0x7f040007
+			public const int titleNAVNmorgen = 2130968583;
+			
+			// aapt resource value: 0x7f04003b
+			public const int titleTAGETaften = 2130968635;
+			
+			// aapt resource value: 0x7f040023
+			public const int titleTAGETmiddag = 2130968611;
+			
+			// aapt resource value: 0x7f04000b
+			public const int titleTAGETmorgen = 2130968587;
 			
 			static Id()
 			{
